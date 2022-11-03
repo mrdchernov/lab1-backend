e2b9b286301de211fcd2cbbb09e23d59bff8051f
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-11-03 23:42:48 +0200

    added middlewares

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const dogsRouter = require('./router/dogs');
 
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+});
+
 const app = express();
 app.use(cors());
-
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()}: url - ${req.url} method = ${req.method}`)
+    next();
+})
 app.use('/api/dog', dogsRouter);
 
 app.listen(3001, () => {
