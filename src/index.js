e15bc44b50335c9ba3d5813cbb3db3d17b451333
Dmytro Chernov <chernov@Dmytros-MacBook-Pro.local>
2022-11-06 20:43:10 +0200

    changed port

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const dogsRouter = require('./router/dogs');
+const config = require('./config');
 
 process.on('unhandledRejection', (err) => {
   console.error(err);
 });
 
 const app = express();
+
 app.use(cors());
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()}: url - ${req.url} method = ${req.method}`)
     next();
-})
+});
+
 app.use('/api/dog', dogsRouter);
 
-app.listen(3001, () => {
-    console.log('listening on port 3001')
+app.listen(config.port, () => {
+    console.log('listening on port ' + config.port)
 });
